79d9f10a4eab578fa7a5260af796a324a71c1dce
hailuu <hoanghai.luu.71@gmail.com>
2023-07-08 12:51:50 +0700

    add mini-mÃax sum algorithm

---
+++ Mini-Max Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        Int64 min = 0;
+        Int64 max = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (i >= 1) max += arr[i];
+            if (i <= 3) min += arr[i];
+        }
+        Console.WriteLine(min + " " + max);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
