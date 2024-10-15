d11ee71cc7ffc514e5efdfd0650c1d1824facaa2
HAILUU <hoanghai.luu.71@gmail.com>
2024-10-15 08:19:52 +0700

    feat: add Escape the knight & Persistent Bugger

---
+++ Escape the knight/Program.cs
@@ -0,0 +1,13 @@
+﻿// ma giáo vãi ò
+static string[] ChooseKingMoves(string kingSquare, string knightSquare, int m)
+{
+    (int c, int r) k = (kingSquare[0] - 97, kingSquare[1] - 49);
+    (int c, int r) n = (knightSquare[0] - 97, knightSquare[1] - 49);
+
+    var r = (k.r & ~1 | ((n.c ^ n.r ^ k.c) & 1)) + 1;
+    var o = new string[] { $"{(char)((k.c ^ 1) + 97)}{r}", $"{(char)((k.c) + 97)}{r}" };
+
+    return Enumerable.Range(0, m).Select(i => o[i%2]).ToArray();
+}
+
+Console.WriteLine(string.Join(", ", ChooseKingMoves("a1", "f3", 20)));
---
+++ Persistent Bugger/Program.cs
@@ -0,0 +1,33 @@
+﻿static int Persistence(long n)
+{
+    if (n < 10) return 0;
+    var count  = 0;
+    var temp   = n;
+    
+    while (temp >= 10)
+    {
+        temp  =  GetDigitMul(temp);
+        count += 1;
+    }
+
+    return count;
+
+    int GetDigitMul(long value)
+    {
+        var result = 1;
+        while (value > 0)
+        {
+            var digit = value % 10; 
+            result *= (int)digit;        
+            value  /= 10;            
+        }
+
+        return result;
+    }
+}
+
+Console.WriteLine($"Count: {Persistence(39)}");
+Console.WriteLine($"Count: {Persistence(4)}");
+Console.WriteLine($"Count: {Persistence(25)}");
+Console.WriteLine($"Count: {Persistence(999)}");
+Console.WriteLine($"Count: {Persistence(123)}");
