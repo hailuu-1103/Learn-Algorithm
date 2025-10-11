ded25c98e068374ef8f48592652ef30c7e7eeaa9
hailuu <hoanghai.luu.71@gmail.com>
2025-10-11 23:11:31 +0700

    ez

--- Birthday Cake Candles/Program.cs
+++ Birthday Cake Candles/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+static int birthdayCakeCandles(List<int> candles)
+{
+    return candles.Count(c => c == candles.Max());
+}
