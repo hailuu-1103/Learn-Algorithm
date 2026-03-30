ca2c11cb09e2ae0960e35e018ba165bdf4b98e63
hailuu <hoanghai.luu.71@gmail.com>
2026-03-30 22:36:16 +0700

    add split a string in balanced string solution

---
+++ Split a String in Balanced Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(BalancedStringSplit("RLRRLLRLRL"));
+
+int BalancedStringSplit(string s)
+{
+    var result = 0;
+    var count  = 0;
+    foreach (var c in s)
+    {
+        switch (c)
+        {
+            case 'R': count++; break;
+            case 'L': count--; break;
+        }
+        if (count == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
