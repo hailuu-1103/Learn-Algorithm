39572a7233c8f868132ed88df461c5ef02d6e5ab
hailuu <hoanghai.luu.71@gmail.com>
2026-03-30 20:27:53 +0700

    chore: add fibonacci and distribute candies solutions

---
+++ DistributeCandies/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var arr = new int[]
+{
+    1, 1, 2, 3,
+};
+Console.WriteLine(DistributeCandies(arr));
+
+int DistributeCandies(int[] candyType)
+{
+    var dict = new Dictionary<int, int>();
+    foreach (var t in candyType)
+    {
+        if (!dict.TryAdd(t, 1))
+        {
+            dict[t]++;
+        }
+    }
+    return Math.Min(candyType.Length / 2, dict.Count);
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(Fib(30));
+
+int Fib(int n)
+{
+    return n switch
+    {
+        0 => 0,
+        1 => 1,
+        _ => Fib(n - 1) + Fib(n - 2),
+    };
+}
